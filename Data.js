0271ec2c20e3d0e62eec2e2e6e27c2870cf6e976
shivank mishra <shivankmishra710@gmail.com>
2024-01-30 18:50:57 +0530

    Deploy to GitHub Pages

---
+++ Data.js
@@ -0,0 +1,29 @@
+const Data =[
+    {
+        id:1,
+        projectName:"my own keeperapp",
+        Linking:"https://my-own-keeperapp.vercel.app/",
+        image:"src/assets/images/cardsImage/1.png",
+        githubLink:"https://github.com/ShivankKMishra/myOwnKeeperapp",
+
+
+    },
+    {
+        id:2,
+        projectName:"currency-converter react",
+        Linking:"https://currency-converter-react-phi.vercel.app/",
+        image:"src/assets/images/cardsImage/2.png",
+        githubLink:"https://github.com/ShivankKMishra/currencyConverter-react",
+
+    },
+    {
+        id:3,
+        projectName:"password Genrator ",
+        Linking:"https://password-genrator-jet.vercel.app/",
+        image:"src/assets/images/cardsImage/3.png",
+        githubLink:"https://github.com/ShivankKMishra/passwordGenrator",
+
+    }
+];
+
+export default Data ;
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    terserOptions: {
+      compress: {
+        // Disable warnings about the use of eval
+        warnings: false,
+        // other compress options...
+      },
+    },
+  },
 })
